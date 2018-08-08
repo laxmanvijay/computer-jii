707e0e76e132a8c8abd0abe4cc0c4fa1ec25542d
laxmanvijay <laxmanvijay24@gmail.com>
2018-08-08 22:15:02 +0530

    half of training part models created

--- controller/DecisionTreeController.py
+++ controller/DecisionTreeController.py
@@ -1,2 +1,50 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from .helper.extension_checker import allowed_file
+from sklearn.externals import joblib
+import json
+import numpy as np
+import pandas as pd
+
+
 def DecisionTreeController(request):
-    return request.form['value']
+    user_name = request.form['user_name']
+    model_name = request.form['model_name']
+    
+    if request.form['process']=='train':
+        dataset = request.files['data']
+        if not allowed_file(dataset.filename):
+            return 'file not allowed'
+        x_features = request.form['x_features']
+        x_features = properify_str(x_features)
+        y_features = request.form['y_features']
+        test_size = float(request.form['test_size'])
+        
+        df = pd.read_csv(dataset)
+        X_data = pd.DataFrame()
+        for i in range(len(x_features)):
+            X_data[x_features[i]] = df[x_features[i]]
+        label = df[y_features]
+        
+        X_train, X_test, y_train, y_test = train_test_split(X_data, label, test_size=test_size, random_state=101)
+        
+        lr = DecisionTreeClassifier()
+        lr.fit(X_train,y_train)
+        joblib.dump(lr,'trained_models/'+'decision_tree_model'+user_name+model_name+'.pkl')
+        return 'Ok model trained'
+    
+    x = request.form['x']
+    x = properify_float(x)
+    lr = joblib.load('trained_models/'+'decision_tree_model'+user_name+model_name+'.pkl')
+    return " ".join(list(map(str,lr.predict(np.array(x).reshape(1,-1)))))
+
+def properify_str(features):
+    t = features.split(',')
+    print(t)
+    return t
+
+def properify_float(features):
+    t = features.split(',')
+    t = list(map(float,t))
+    print(t)
+    return t
--- controller/LinearRegressionController.py
+++ controller/LinearRegressionController.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from .helper.extension_checker import allowed_file
 from sklearn.externals import joblib
 import json
 import numpy as np
@@ -7,27 +8,44 @@ import pandas as pd
 
 
 def LinearRegressionController(request=None):
+    
     user_name = request.form['user_name']
     model_name = request.form['model_name']
+    
     if request.form['process']=='train':
+        dataset = request.files['data']
+        if not allowed_file(dataset.filename):
+            return 'file not allowed'
         x_features = request.form['x_features']
-        x_features = properify(x_features)
+        x_features = properify_str(x_features)
         y_features = request.form['y_features']
         test_size = float(request.form['test_size'])
-        dataset = request.files['data']
+        
         df = pd.read_csv(dataset)
-        X_data = df[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms','Avg. Area Number of Bedrooms', 'Area Population']]
+        X_data = pd.DataFrame()
+        for i in range(len(x_features)):
+            X_data[x_features[i]] = df[x_features[i]]
         label = df[y_features]
+        
         X_train, X_test, y_train, y_test = train_test_split(X_data, label, test_size=test_size, random_state=101)
+        
         lr = LinearRegression()
         lr.fit(X_train,y_train)
         joblib.dump(lr,'trained_models/'+'linear_model'+user_name+model_name+'.pkl')
         return 'Ok model trained'
+    
     x = request.form['x']
+    x = properify_float(x)
     lr = joblib.load('trained_models/'+'linear_model'+user_name+model_name+'.pkl')
-    return " ".join(list(map(str,lr.predict(x))))
+    return " ".join(list(map(str,lr.predict(np.array(x).reshape(1,-1)))))
 
-def properify(features):
+def properify_str(features):
     t = features.split(',')
+    print(t)
+    return t
+
+def properify_float(features):
+    t = features.split(',')
+    t = list(map(float,t))
     print(t)
     return t
