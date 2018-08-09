e6d33a84d5f6f065baa9ff906b208f6faf3263b5
laxmanvijay <laxmanvijay24@gmail.com>
2018-08-09 21:42:24 +0530

    ex

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask,request
 from controller import MainController
+from database_connector.connector import connect
 
 app = Flask(__name__)
+
+db = connect()
 
 MainController.MainController(app, request)
 
